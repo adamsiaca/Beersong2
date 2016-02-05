24b5ded3808efe23cfa8c4c1eb628d16c9d007cb
Adam O'Donnell <arodonne@oakland.edu>
2016-02-04 22:58:16 -0500

    beersong

--- src/com/Beersong/Beersong.java
+++ src/com/Beersong/Beersong.java
@@ -11,6 +11,19 @@ public class Beersong {
     public static void Ninety_Nine_Bottles_of_Beer() {
 
 
-        
+        for (int i = 99; i > 0; i--) {
+            if (i == 1) {
+                System.out.println("1 bottle of beer on the wall, 1 bottle of beer");
+                System.out.println("take one down, pass it around, no more bottles of beer on the wall.");
+            }
+            else if (i == 2) {
+                System.out.println("2 bottles of beer on the wall, 2 bottles of beer");
+                System.out.println("take one down, pass it around, 1 more bottle of beer on the wall.");
+            }
+            else {
+                System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer");
+                System.out.println("take one down, pass it around, " + (i - 1) + " bottles of beer on the wall.");
+            }
+        }
     }
 }
